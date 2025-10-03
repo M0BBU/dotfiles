63db0f77f7a856b84de5bf24e7b21da9f56bde5a
Daniel Choo <danielchoo@uber.com>
2025-10-03 15:07:16 -0700

    update hammerspoon and remove .zshrc

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,9 +1,24 @@
+hs.settings.set("1", "Firefox")
+hs.settings.set("2", "Emacs")
+hs.settings.set("3", "Ghostty")
+hs.settings.set("4", "Bitwarden")
+
+-- Check if a settings.lua file exists and if not, skip. This lets me apply any
+-- customizations I need for work without needing to modify my personal settings.
+local settings_file = hs.configdir .. "/settings.lua"
+local settings = io.open(settings_file, "r")
+if settings then
+	dofile(settings_file)
+else
+	print("Settings file not found. Skipping...")
+end
+
 local apps =
 {
-	["1"] = "Firefox",
-	["2"] = "emacs",
-	["3"] = "Ghostty",
-	["4"] = "Bitwarden",
+	["1"] = hs.settings.get("1"),
+	["2"] = hs.settings.get("2"),
+	["3"] = hs.settings.get("3"),
+	["4"] = hs.settings.get("4"),
 }
 for key, app in pairs(apps) do
 	hs.hotkey.bind({"cmd", "ctrl"}, key, function()
--- .zshrc
+++
@@ -1,4 +0,0 @@
-alias config='/usr/bin/git --git-dir=$HOME/.mycfg/ --work-tree=$HOME'
-alias bupdate="brew update &&\
-    brew bundle install --cleanup --file=~/.config/Brewfile &&\
-    brew upgrade"
