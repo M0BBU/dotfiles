739b364fd8c56d0ebab28c79980e17b2739b8b4d
Daniel Choo <danielchoo@uber.com>
2025-12-17 08:40:39 -0500

    more fun git aliases

--- .config/zsh/config.zsh
+++ .config/zsh/config.zsh
@@ -1,8 +1,5 @@
 eval "$(direnv hook zsh)"
 
-export EDITOR="vim"
-bindkey -M viins 'jk' vi-cmd-mode
-
 source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 source <(fzf --zsh)
 
@@ -26,6 +23,11 @@ if ! type "gs" >/dev/null 2>/dev/null; then
     go build -o "$HOME/.bin"
 fi
 
+# fun git config
+git config --global branch.autoSetupMerge always
+git config --global spice.branchCreate.prefix $(whoami)/
+
+
 alias config='/usr/bin/git --git-dir=$HOME/.mycfg/ --work-tree=$HOME'
 alias bupdate="brew update &&\
     brew bundle install --file=~/.config/Brewfile &&\
@@ -36,7 +38,7 @@ alias arcd='arc diff --noautoland --amend-all --apply-patches --allow-untracked 
 alias arcl='arc lint --apply-patches'
 alias update='gca && arhp --no-interactive'
 
-alias ga='git all -A'
+alias gaa='git all -A'
 alias gcm='git commit -m'
 alias grm='git rebase -i main'
 alias grc='git rebase --continue'
@@ -45,23 +47,24 @@ alias gst='git status'
 alias gbup=rebase_branch
 
 alias arhp='arh publish --apply-fixes'
-alias gbc=git_branch_create
-alias gcc='gs cc -a -m'
+alias gcc=create_commit
 alias gca='gs ca -a --no-edit'
 alias resync='gs rs && gs rr'
-function git_branch_create() {
+
+alias gstash='git add -A && git commit -m "TEMPORARY STASH. DO NOT COMMIT."'
+alias gpop='check_git_pop'
+
+function create_commit() {
     if [ $# -eq 0 ]; then
         echo "No arguments provided. Exiting."
         return 1
     fi
 
     parent="$(git rev-parse --abbrev-ref HEAD)"
-    gs bc $(whoami)/$1 --no-commit
+    gs bc -a -m $1
     git branch --set-upstream-to=$parent
 }
 
-alias gstash='git add -A && git commit -m "TEMPORARY STASH. DO NOT COMMIT."'
-alias gpop='check_git_pop'
 function check_git_pop() {
     if [ "$(git log -1 --pretty=%B)" = "TEMPORARY STASH. DO NOT COMMIT." ]; then
         git reset HEAD~1
